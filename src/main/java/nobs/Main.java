74126ac555790cffa2b595d778bac8cc18b87d39
Sveinung Dalatun <sdalatun@gmail.com>
2013-02-09 15:13:12 +0100

    Start the jetty server

--- src/main/java/nobs/Main.java
+++ src/main/java/nobs/Main.java
@@ -1,6 +1,19 @@
 package nobs;
 
+import org.eclipse.jetty.server.Server;
+
 public class Main {
     public static void main(String[] args) {
+        Server server = new Server(8080);
+
+        server.setStopAtShutdown(true);
+
+        try {
+            server.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 }
