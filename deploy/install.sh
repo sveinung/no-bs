6fd025c292312edaf373d4e1d981993bb4e27ccc
Sveinung Dalatun <sdalatun@gmail.com>
2013-03-12 20:29:48 +0100

    Remove duplicated variables in deployment and service scripts

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-APP_NAME=no-bs
-APP_LOCATION="/opt/${APP_NAME}"
+pushd /vagrant/
+
+source deploy/daemon-functions.sh
+
 APP_JAR="/vagrant/build/libs/${APP_NAME}.jar"
+APP_LIB_DIR="${APP_LOCATION}/lib"
 
 echo "Create app folder"
 sudo mkdir -p $APP_LOCATION
@@ -17,5 +20,10 @@ fi
 
 echo "Installing service script..."
 sudo cp /vagrant/deploy/init.d/no-bs /etc/init.d/
+
+sudo mkdir -p $APP_LIB_DIR
+sudo cp deploy/daemon-functions.sh $APP_LIB_DIR/
+
+popd
 
 exit 0
