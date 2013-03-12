1a1d3f651395f5dc4e112a266b3e4c7710c6d3d2
Sveinung Dalatun <sdalatun@gmail.com>
2013-03-12 19:35:29 +0100

    Remove unused variables

--- deploy/install.sh
+++ deploy/install.sh
@@ -3,8 +3,6 @@
 APP_NAME=no-bs
 APP_LOCATION="/opt/${APP_NAME}"
 APP_JAR="/vagrant/build/libs/${APP_NAME}.jar"
-LOGS=/var/log/${APP_NAME}
-PID_FILE="/var/run/${APP_NAME}.pid"
 
 echo "Create app folder"
 sudo mkdir -p $APP_LOCATION
