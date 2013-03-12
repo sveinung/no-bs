349f5581edc3e7e0dd15ac82c069569ac9b478ad
Sveinung Dalatun <sdalatun@gmail.com>
2013-03-12 19:25:40 +0100

    Add service script

--- deploy/install.sh
+++ deploy/install.sh
@@ -3,6 +3,8 @@
 APP_NAME=no-bs
 APP_LOCATION="/opt/${APP_NAME}"
 APP_JAR="/vagrant/build/libs/${APP_NAME}.jar"
+LOGS=/var/log/${APP_NAME}
+PID_FILE="/var/run/${APP_NAME}.pid"
 
 echo "Create app folder"
 sudo mkdir -p $APP_LOCATION
@@ -14,3 +16,8 @@ else
   echo "${APP_JAR} not found. Build it!"
   return 1
 fi
+
+echo "Installing service script..."
+sudo cp /vagrant/deploy/init.d/no-bs /etc/init.d/
+
+exit 0
