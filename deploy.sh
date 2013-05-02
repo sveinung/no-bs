8e5f5edf9d9a717d3d75f50ded3a684f4bb1c2dc
Sveinung Dalatun <sdalatun@gmail.com>
2013-05-02 22:01:05 +0200

    Transfer the deploy scripts to the host

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,12 @@ SSH_SOCKET=~/.ssh/$USER@$HOST
 ssh -M -f -N -p $PORT -o ControlPath=$SSH_SOCKET $USER@$HOST
 
 scp -o ControlPath=$SSH_SOCKET build/libs/$APP_NAME.jar $USER@$HOST:/home/$USER/
+rsync -e "ssh -o 'ControlPath=${SSH_SOCKET}'" \
+    --recursive \
+    --verbose \
+    --delete \
+    --executability \
+    deploy/ $HOST:/home/$USER/deploy/
 
 ssh -S $SSH_SOCKET -O exit $USER@$HOST
 
