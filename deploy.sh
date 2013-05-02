0fa94ded982da2277cb12bc910691a0dba49d15c
Sveinung Dalatun <sdalatun@gmail.com>
2013-05-02 21:49:09 +0200

    Transfer the .jar over ssh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-vagrant ssh -c "/vagrant/deploy/install.sh"
+APP_NAME=no-bs
+
+USER=vagrant
+HOST=localhost
+PORT=2222
+SSH_SOCKET=~/.ssh/$USER@$HOST
+
+ssh -M -f -N -p $PORT -o ControlPath=$SSH_SOCKET $USER@$HOST
+
+scp -o ControlPath=$SSH_SOCKET build/libs/$APP_NAME.jar $USER@$HOST:/home/$USER/
+
+ssh -S $SSH_SOCKET -O exit $USER@$HOST
+
