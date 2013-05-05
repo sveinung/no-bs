52d2d602022519ade1013dd2ed885ecd113bb0f3
Sveinung Dalatun <sdalatun@gmail.com>
2013-05-05 16:25:13 +0200

    Add some more stubbed books

--- src/main/java/nobs/book/BookRepository.java
+++ src/main/java/nobs/book/BookRepository.java
@@ -8,6 +8,10 @@ public class BookRepository {
                 return new Book("John Steinbeck", "Of Mice and Men");
             case 2:
                 return new Book("Isaac Asimov", "The Last Question");
+            case 3:
+                return new Book("Moby Dick", "Herman Melville");
+            case 4:
+                return new Book("Sult", "Knut Hamsun");
             default:
                 return new Book();
         }
--- src/main/java/nobs/library/LibraryRepository.java
+++ src/main/java/nobs/library/LibraryRepository.java
@@ -9,7 +9,10 @@ public class LibraryRepository {
     public Library getLibrary(int libraryId) {
         switch (libraryId) {
             case 1:
-                return new Library(asList(new BookShort("Of Mice and Men", new BookID(1))));
+                return new Library(asList(new BookShort("Of Mice and Men", new BookID(1)),
+                                          new BookShort("The Last Question", new BookID(2)),
+                                          new BookShort("Moby Dick", new BookID(3)),
+                                          new BookShort("Sult", new BookID(4))));
             case 2:
                 return new Library(asList(new BookShort("Of Mice and Men", new BookID(1)),
                                           new BookShort("The Last Question", new BookID(2))));
