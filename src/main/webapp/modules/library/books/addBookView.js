b160def741b970329386edf25270a99a5d7a8949
Sveinung Dalatun <sdalatun@gmail.com>
2013-05-26 17:34:42 +0200

    Hide the AddBookView when clicking cancel

--- src/main/webapp/modules/library/books/addBookView.js
+++ src/main/webapp/modules/library/books/addBookView.js
@@ -7,8 +7,20 @@ define(function(require) {
     var AddBookView = function(options) {
         var el = options.el;
 
+        var cancelButtonClicked = function(event) {
+            event.preventDefault();
+            el.addClass('hide');
+        };
+
+        var submitButtonClicked = function(event) {
+            event.preventDefault();
+        };
+
         var render = function() {
             el.html(mustache.render(template));
+
+            el.find('.cancel-button').click(cancelButtonClicked);
+            el.find('.submit-button').click(submitButtonClicked);
         };
 
         return {
