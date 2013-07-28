d98885c2f2c106d2a860e4ec09e52aa4f04b57d6
Sveinung Dalatun <sdalatun@gmail.com>
2013-07-28 20:49:11 +0200

    Test that the books is shown in the list

--- src/main/webapp/modules/library/libraryViewSpec.js
+++ src/main/webapp/modules/library/libraryViewSpec.js
@@ -1,7 +1,27 @@
 define(function(require) {
+
+    var LibraryView = require('modules/library/libraryView'),
+        LibraryRepository = require('modules/library/libraryRepository'),
+        $ = require('jquery'),
+        sinon = require('sinon'),
+        responseFaker = require('responseFaker');
+
     describe('LibraryView', function() {
         it('has books', function() {
-            expect(true).toBe(true);
+            var el = $('<div></div>');
+
+            var libraryView = LibraryView({
+                el: el,
+                libraryId: 1,
+                libraryRepository: LibraryRepository()
+            });
+
+            var response = {"books":[{"title":"Of Mice and Men","uri":"/book/1"},{"title":"Sult","uri":"/book/2"}]};
+            responseFaker.fakeResponse(response, {}, function() {
+                libraryView.render();
+            });
+
+            expect(el.find(".books li").size()).toBe(2);
         });
     });
 });
