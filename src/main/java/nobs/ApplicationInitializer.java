1a13225d3b56d907a6d68882babe05efa0f53232
Sveinung Dalatun <sdalatun@gmail.com>
2014-06-12 22:00:27 +0200

    Forenkle oppstart av resources

--- src/main/java/nobs/ApplicationInitializer.java
+++ src/main/java/nobs/ApplicationInitializer.java
@@ -1,5 +1,13 @@
 package nobs;
 
+import nobs.book.BookRepository;
+import nobs.book.BookResource;
+import nobs.book.BookURIBuilder;
+import nobs.genre.GenreRepository;
+import nobs.genre.GenreResource;
+import nobs.library.LibraryRepository;
+import nobs.library.LibraryResource;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 
 import javax.servlet.ServletContext;
@@ -15,18 +23,47 @@ public class ApplicationInitializer implements ServletContextListener
     {
         ServletContext servletContext = sce.getServletContext();
 
-        ServletRegistration.Dynamic jersey = servletContext.addServlet("jersey", new ServletContainer());
-        jersey.addMapping("/rest/*");
-
-        HashMap<String, String> initParameters = new HashMap<>();
-        initParameters.put("javax.ws.rs.Application", MyApplication.class.getCanonicalName());
-        initParameters.put("com.sun.jersey.api.json.POJOMappingFeature", "true");
-
-        jersey.setInitParameters(initParameters);
+        initializeJersey(servletContext);
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce)
     {
+    }
+
+    private void initializeJersey(ServletContext servletContext)
+    {
+        ServletRegistration.Dynamic jersey = servletContext.addServlet(
+                "jersey",
+                new ServletContainer(createJerseyResourceConfig()));
+        jersey.addMapping("/rest/*");
+
+        HashMap<String, String> initParameters = new HashMap<>();
+        initParameters.put("com.sun.jersey.api.json.POJOMappingFeature", "true");
+
+        jersey.setInitParameters(initParameters);
+    }
+
+    private ResourceConfig createJerseyResourceConfig()
+    {
+        ResourceConfig resourceConfig = new ResourceConfig();
+
+        BookRepository bookRepository = new BookRepository();
+
+        LibraryResource libraryResource = new LibraryResource(
+                new LibraryRepository(bookRepository),
+                new BookURIBuilder());
+
+        BookResource bookResource = new BookResource(bookRepository);
+
+        GenreResource genreResource = new GenreResource(new GenreRepository());
+
+        resourceConfig.registerInstances(
+                libraryResource,
+                bookResource,
+                genreResource
+        );
+
+        return resourceConfig;
     }
 }
